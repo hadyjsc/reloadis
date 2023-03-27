3c26f4d53fc06b32c59d82d2943db0418775288c
Hady Eka Saputra <hadyekasaputra@gmail.com>
2023-03-27 22:35:03 +0700

    trasaction report table

--- app/Tables/TransactionReportTable.php
+++ app/Tables/TransactionReportTable.php
@@ -21,12 +21,13 @@ class TransactionReportTable extends AbstractTableConfiguration
         return Table::make()
             ->model(Product::class)
             ->query(function(Builder $query) {
-                return $query->select(DB::raw('products.id, products.provider_id, products.price, products.fund,
+                return $query->select(DB::raw('products.id, products.provider_id, providers.name, providers.color, products.price, products.fund,
                     COUNT(product_items.id) AS stock,
                     COUNT(CASE WHEN product_items.is_sold = true THEN product_items.id END) AS sold,
                     COUNT(product_items.id) - COUNT(CASE WHEN product_items.is_sold = true THEN product_items.id END) AS last_stock,
                     (products.price - products.fund) * COUNT(CASE WHEN product_items.is_sold = true THEN product_items.id END) AS profit'))
                 ->join('product_items', 'products.id', '=', 'product_items.product_id')
+                ->join('providers', 'products.provider_id', '=', 'providers.id')
                 ->groupBy(['products.id', 'products.provider_id', 'products.price', 'products.fund'])
                 ->orderBy('products.provider_id');
             });
@@ -35,10 +36,15 @@ class TransactionReportTable extends AbstractTableConfiguration
     protected function columns(): array
     {
         return [
-            Column::make('id')->title('id')->sortable(),
-            Column::make('provider_id')->title('Provider'),
-            Column::make('price')->title('Harga'),
-            Column::make('fund')->title('Modal'),
+            Column::make('name')->title('Provider')->format(function($data) {
+                return '<strong style="color:'.$data['color'].'">'.$data['name'].'</strong>';
+            } ),
+            Column::make('price')->title('Harga')->format(function($data) {
+                return 'Rp. '.number_format($data['price'], 0, ',', '.');;
+            }),
+            Column::make('fund')->title('Modal')->format(function($data) {
+                return 'Rp. '.number_format($data['fund'], 0, ',', '.');;
+            }),
             Column::make('stock')->title('Stok'),
             Column::make('last_stock')->title('Sisa Stok'),
             Column::make('sold')->title('Terjual'),
@@ -51,6 +57,42 @@ class TransactionReportTable extends AbstractTableConfiguration
     protected function results(): array
     {
         return [
+            Result::make()
+                ->title('Total terjual')
+                ->format(static function(BuilderResult $query) {
+                    $query = Product::select(DB::raw('products.id, products.price,
+                        COUNT(CASE WHEN product_items.is_sold = true THEN product_items.id END) AS sold,
+                        (products.price * COUNT(CASE WHEN product_items.is_sold = true THEN product_items.id END)) AS total_sold'))
+                        ->leftjoin('product_items', 'products.id', '=', 'product_items.product_id')
+                        ->groupBy(['products.price', 'products.id'])
+                        ->get(['id', 'sold','total_sold', 'price']);
+
+                    $sum = 0;
+                    foreach ($query as $key => $value) {
+                        $sum += $value->total_sold;
+                    }
+
+                    $toIDR = number_format($sum, 0, ',', '.');
+
+                    return 'Rp. '.$toIDR;
+            }),
+            Result::make()
+                ->title('Total Modal')
+                ->format(static function(BuilderResult $query) {
+                    $query = Product::select(DB::raw('products.id, products.fund, COUNT(product_items.id) AS stock, (products.fund * COUNT(product_items.id)) AS total_fund'))
+                        ->leftjoin('product_items', 'products.id', '=', 'product_items.product_id')
+                        ->groupBy(['products.fund', 'products.id'])
+                        ->get(['id', 'fund','stock', 'total_fund']);
+
+                    $sum = 0;
+                    foreach ($query as $key => $value) {
+                        $sum += $value->total_fund;
+                    }
+
+                    $toIDR = number_format($sum, 0, ',', '.');
+
+                    return 'Rp. '.$toIDR;
+            }),
             Result::make()
                 ->title('Total profit yang diperoleh')
                 ->format(static function(BuilderResult $query) {
@@ -67,7 +109,7 @@ class TransactionReportTable extends AbstractTableConfiguration
                     $toIDR = number_format($sum, 0, ',', '.');
 
                     return 'Rp. '.$toIDR;
-                }),
+            }),
         ];
     }
 }
--- resources/views/transaction/index.blade.php
+++ resources/views/transaction/index.blade.php
@@ -6,8 +6,24 @@
             <h1>Rekap Transaksi</h1>
         </div>
         <div class="row">
-            <div class="col-xl-12 col-md-12 col-sm-12">
-                <livewire:table :config="App\Tables\TransactionReportTable::class"/>
+            <div class="col-lg-12">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-lg-12 col-md-12">
+                                <a href="#" class="btn btn-icon icon-left btn-success"><i class="fas fa-file-excel"></i> Export ke Excel</a>
+                                <a href="#" class="btn btn-icon icon-left btn-success"><i class="fas fa-file-pdf"></i> Export ke PDF</a>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-lg-12 col-md-12">
+                                <div class="col-xl-12 col-md-12 col-sm-12">
+                                    <livewire:table :config="App\Tables\TransactionReportTable::class"/>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </section>
